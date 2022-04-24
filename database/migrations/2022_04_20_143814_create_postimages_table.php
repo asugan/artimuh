e9aa815537821bf3922c208c4565708110176b14
asugan <cagatayeren1898@gmail.com>
2022-04-25 00:39:41 +0300

    changes

--- database/migrations/2022_04_20_143814_create_postimages_table.php
+++ database/migrations/2022_04_20_143814_create_postimages_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('postimages', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('baslik');
             $table->text('text');
             $table->string('basimage')->nullable();
